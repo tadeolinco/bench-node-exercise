1780f160f4f450d1f8089abf3a904c6370f94ca7
Sam Bautista <tadeolinco@gmail.com>
2018-07-05 19:33:45 +0800

    Add SSL for pg

--- src/db/index.js
+++ src/db/index.js
@@ -6,6 +6,7 @@ const pool = new pg.Pool({
   database: 'd7la61fqjg2sou',
   password: '78fa3ce70abc039fdc025dc6259b4a05428180a04428d11d17eb804b2d76d6b5',
   port: 5432,
+  ssl: true,
 })
 console.log('Successfully connected to DB')
 
